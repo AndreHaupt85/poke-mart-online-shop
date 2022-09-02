92636e6dbca37a8a1b163a697393d5901a4260d1
AlexSna <asnatschko@gmail.com>
2022-09-02 16:10:04 +0200

    bugfix image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { useEffect, useState } from 'react';
 import Header from './components/Header';
-import ItemMainFetch from './components/ItemMainFetch';
+import ItemSubFetch from './components/ItemSubFetch';
 
 function App() {
 
+  const apiURL = "https://pokeapi.co/api/v2/item/";
 
-  
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+      fetch(apiURL)
+          .then((response) => response.json())
+          .then((data) => { setItems(data.results); })
+          .catch((error) => console.error(error));
+  }, [apiURL]);
+
+
   return (
     <div className="App">
       <Header />
-      <ItemMainFetch />
+      
+      {items.map((item) => {
+                return <ItemSubFetch key={item.name} pokeitem={item}/>
+            })}
       
     </div>
   );
