5ef58dbe80d3ade9ccc8f3769273e724fc61dcee
Andre Haupt <andrehaupt@me.com>
2022-09-04 12:51:17 +0200

    reset2

--- src/App.js
+++ src/App.js
@@ -1,47 +1,66 @@
-import './App.css';
+import { useEffect, useState } from 'react';
+import Header from './components/Header';
+import ItemSubFetch from './components/ItemSubFetch';
 
 function App() {
+
+  const apiURL = "https://pokeapi.co/api/v2/item/";
+
+  const [items, setItems] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    fetch(apiURL)
+      .then((response) => response.json())
+      .then((data) => { setItems(data.results); })
+      .catch((error) => console.error(error));
+  }, [apiURL]);
+
+
+
+
+  function addButton(pokemonName) {
+    const theItem = items.find((item) => {
+      return item.name === pokemonName;
+    }); (
+      items.filter((item) => {
+        return item.name !== theItem.name;
+      })
+    );
+    setCartItems([theItem, ...cartItems]);
+    // setCounter(counter + 1);
+  }
+
+  function removeButton(pokemonName) {
+    const theItem = cartItems.find((item) => {
+      return item.name === pokemonName;
+    });
+
+    setCartItems(
+      cartItems.filter((item) => {
+        return item.name !== theItem.name;
+      })
+    );
+
+    setItems([theItem, ...items]);
+    // setCounter(counter - 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Header />
+      <h2>Card</h2>
+      {cartItems.map((item) => {
+        return <ItemSubFetch key={item.name} onRemoveButton={removeButton} pokeitem={item} />
+      })}
+
+      <h2>Search Items</h2>
+      {items.map((item) => {
+        return <ItemSubFetch key={item.name} onAddButton={addButton} pokeitem={item} />
+      })}
+
     </div>
   );
 }
-
-
-  // verschieben
-
- 
-
-  // return (
-  //   <main className="App__container">
-  //     <h1 className="App__title">Pokédex</h1>
-  //     <div>
-  //       <h2>
-  //         You have caught {cardItems.length} out of {pokemons.length}{" "}
-  //         Pokémon!
-  //       </h2>
-        
-  //     </div>
-  //     <div>
-  //       <h2>You can still catch some Pokémon</h2>
-  //       {searchItems.map((pokemon) => {
-  //         return (
-  //           <Card
-  //             key={pokemon.id}
-  //             title={pokemon.name}
-  //             image={pokemon.image}
-  //             types={pokemon.types}
-  //             onAddCounter={addCounter}
-  //             onSubtractCounter={subtractCounter}
-  //           />
-  //         );
-  //       })}
-  //     </div>
-  //   </main>
-  // );
-    
-
 
 export default App;
--- src/components/ItemSubFetch.js
+++ src/components/ItemSubFetch.js
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 
 
 
-export default function ItemSubFetch({pokeitem, onAddButton, onRemoveButton}) {
-console.log(pokeitem)
+export default function ItemSubFetch({ pokeitem, onAddButton, onRemoveButton }) {
+    console.log(pokeitem)
     const subDataURL = pokeitem.url;
 
     const [subDatas, setSubDatas] = useState({
@@ -14,33 +14,30 @@ console.log(pokeitem)
 
     useEffect(() => {
         fetch(subDataURL)
-        .then((response) => response.json())
-        .then ((data) => { 
-            setSubDatas(data)
-        })
-        .catch((error)=> console.error(error));
+            .then((response) => response.json())
+            .then((data) => {
+                setSubDatas(data)
+            })
+            .catch((error) => console.error(error));
     }, [subDataURL]);
 
     function handleAddClick() {
         onAddButton(pokeitem.name);
-      }
-      
-      function handleRemoveClick() {
+    }
+
+    function handleRemoveClick() {
         onRemoveButton(pokeitem.name);
-      }
-    
-
-
+    }
 
     return (
         <div className="card">
-                        <div key={subDatas.name}>
-                            <div>{pokeitem.name}</div>
-                            {subDatas.sprites.default ? <img alt="" src={subDatas.sprites.default} /> : ''}
-                            <div>{subDatas.cost}</div> 
-                            <button type="button" onClick={handleAddClick}>Add Item</button> 
-                            <button type="button" onClick={handleRemoveClick}>Remove Item</button>          
-                        </div>
+            <div key={subDatas.name}>
+                <div>{pokeitem.name}</div>
+                {subDatas.sprites.default ? <img alt="" src={subDatas.sprites.default} /> : ''}
+                <div>{subDatas.cost}</div>
+                <button type="button" onClick={handleAddClick}>Add Item</button>
+                <button type="button" onClick={handleRemoveClick}>Remove Item</button>
+            </div>
         </div>
     )
-}
+};
